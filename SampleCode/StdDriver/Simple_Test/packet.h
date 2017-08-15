ff96352ee2f9181f656fb9c9395598380984cc67
MHLEE-PC\mhlee <redmine4u@gmail.com>
2017-08-16 00:30:54 +0900

    added packet sources

---
+++ SampleCode/StdDriver/Simple_Test/packet.h
@@ -0,0 +1,22 @@
+#ifndef __PACKET_H__
+#define __PACKET_H__
+
+#include "system.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	
+	
+
+	
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PACKET_H__ */
+
+/* EOF */
+
