0fc4b66db44f66a7e75649d3c0235bc0e0c022e0
MHLEE-PC\mhlee <redmine4u@gmail.com>
2017-08-22 23:55:08 +0900

    packet.h added

--- SampleCode/StdDriver/Simple_Test/packet.h
+++ SampleCode/StdDriver/Simple_Test/packet.h
@@ -7,8 +7,38 @@
 extern "C"
 {
 #endif
-	
-	
+
+enum {
+	PKT_TYPE_A,
+	PKT_TYPE_B
+};
+
+typedef struct {
+	uint8_t gas;	// Gas sonsor
+	uint8_t temp;	// Temperature sonsor
+	uint8_t hum;	// Humidity Sensor
+	uint8_t	ult; 	// Ultrasonic sensor
+	uint8_t dust;	// Dust
+} sensor_t;
+
+typedef struct {
+	uint8_t adc[5];
+	uint8_t sw[10];
+} input_t;
+
+typedef struct {
+	char date[10];
+	char time[8];
+	uint32_t seqNum;
+	char phoneNumber[11];
+	char modemId[10];
+	char boardId[10];
+	uint32_t fwVer;  // 0xFFFFFFFF
+	char fwDate[10]; // build date
+	char gps[10];
+	sensor_t sensor;
+	input_t input;
+} packet_t;
 
 	
 	
